e3d76c230d5b051fe37957864d84035bd21a5d0f
Juan Sebastián Osorio García <seposorio@hotmail.com>
2021-01-20 12:27:02 -0500

    Custom hook for fetching data -> re-usability

--- usersettingspage/src/components/Home.js
+++ usersettingspage/src/components/Home.js
@@ -1,39 +1,21 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import UserCard from './UserCard'
 import '../css/Home.css'
+import useFetch from '../useFetch'
 
 
 function Home() {
-    const [customers, setcustomers] = useState([])
-    const [error, seterror] = useState(null)
-
-    useEffect(() => {
-        fetch('/api/v1/customerdata/')
-            .then(res => {
-                if(!res.ok){
-                    throw Error('Can not fetch the data')
-                }
-                return res.json();
-            })
-            .then(data => {
-                console.log(data.results);
-                setcustomers(data.results);
-            })
-            .catch(e=>{
-                console.log(e.message);
-                seterror(e.message)
-            })
-    }, [])
+   const{data: customers,error}=useFetch('/api/v1/customerdata/')
+   
     return (
         <div className='home'>
             <h1>Our Customers</h1>
-            {error && <div> {error}</div>  }
+            {error && <div> {error}</div>}
             <div className="home__userscontainer">
                 {customers.map((customer, index) => (
-                    <UserCard key={customer.id} name={customer.data.theme_name} email={customer.data.user_email} imgsrc={customer.data.user_profile_image}/>
+                    <UserCard key={customer.id} name={customer.data.theme_name} email={customer.data.user_email} imgsrc={customer.data.user_profile_image} />
                 ))
                 }
-
             </div>
         </div>
     )
