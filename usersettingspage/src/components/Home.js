13059c6a07e205bac6387e0e3f257f8d5fd9ac9b
Juan Sebastián Osorio García <seposorio@hotmail.com>
2021-01-20 01:35:51 -0500

    Fetch error handling in useEffect

--- usersettingspage/src/components/Home.js
+++ usersettingspage/src/components/Home.js
@@ -5,21 +5,29 @@ import '../css/Home.css'
 
 function Home() {
     const [customers, setcustomers] = useState([])
+    const [error, seterror] = useState(null)
 
     useEffect(() => {
         fetch('/api/v1/customerdata/')
             .then(res => {
+                if(!res.ok){
+                    throw Error('Can not fetch the data')
+                }
                 return res.json();
             })
             .then(data => {
-                console.log(data.results)
-                setcustomers(data.results)
+                console.log(data.results);
+                setcustomers(data.results);
+            })
+            .catch(e=>{
+                console.log(e.message);
+                seterror(e.message)
             })
     }, [])
     return (
         <div className='home'>
             <h1>Our Customers</h1>
-
+            {error && <div> {error}</div>  }
             <div className="home__userscontainer">
                 {customers.map((customer, index) => (
                     <UserCard key={customer.id} name={customer.data.theme_name} email={customer.data.user_email} imgsrc={customer.data.user_profile_image}/>
