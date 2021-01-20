5d774ffbf1c156ec3de4c3a0998d948fbbe09282
Juan Sebastián Osorio García <seposorio@hotmail.com>
2021-01-19 20:54:23 -0500

    Sending card values as props

--- usersettingspage/src/components/Home.js
+++ usersettingspage/src/components/Home.js
@@ -2,15 +2,12 @@ import React from 'react'
 import UserCard from './UserCard'
 import '../css/Home.css'
 function Home() {
-    
+
     return (
         <div className='home'>
             <h1>Our Customers</h1>
             <div className="home__userscontainer">
-                <UserCard />
-                <UserCard />
-                <UserCard />
-                <UserCard />
+                <UserCard name={'Obama'} email={"gmail.com"}/>
             </div>
         </div>
     )
--- usersettingspage/src/components/UserCard.js
+++ usersettingspage/src/components/UserCard.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import '../css/UserCard.css'
 
-function UserCard() {
+function UserCard({name,email}) {
     return (
         <div className='usercard'>
             <div className="usercard__header">
                 <img src="https://i.imgur.com/LMhM8nn.jpg" alt="" />
             </div>
             <div className="usercard__body">
-                <h1>Super Obamas</h1>
-                <h5>gmail@hotmail.com</h5>
+                <h1>{name}</h1>
+                <h5>{email}</h5>
                 <button><span>SETTINGS</span></button>
             </div>
         </div>
