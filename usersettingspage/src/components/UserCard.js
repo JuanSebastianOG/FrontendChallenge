8ab77308472afa60217cd5f1598626094e2ae3e7
Juan Sebastián Osorio García <seposorio@hotmail.com>
2021-01-19 20:48:53 -0500

    Customer card style done

--- usersettingspage/src/components/UserCard.js
+++ usersettingspage/src/components/UserCard.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import '../css/UserCard.css'
 
 function UserCard() {
     return (
-        <div>
-            <h1>UserCard</h1>
+        <div className='usercard'>
+            <div className="usercard__header">
+                <img src="https://i.imgur.com/LMhM8nn.jpg" alt="" />
+            </div>
+            <div className="usercard__body">
+                <h1>Super Obamas</h1>
+                <h5>gmail@hotmail.com</h5>
+                <button><span>SETTINGS</span></button>
+            </div>
         </div>
     )
 }
